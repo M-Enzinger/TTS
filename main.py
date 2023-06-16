07d389cfed03b9729b42fddb3b999daf20ce0014
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2023-06-16 13:08:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,29 +19,35 @@ with tab1:
   st.header("Google Text To Speech")
   st.info("Step 1: Type in your text")
   text_val = st.text_area('Text to convert to speech')
-  language = 'de'
+  language_option = st.selectbox(
+    'To which language to you want to convert to? (more possible)',
+    ('English', 'German', 'French (for whatever reason)', 'Spanish'))
+  lang_dict = {'English':'en', 'German':'de', 'French':'fr', 'Spanish':'es'}
+  language = lang_dict[language_option]
   st.info("Step 2: Press 'Convert To Speech'")
-  if st.button('Convert to Speech'):
-    tts = tts(text_val, language)
-    if (tts is not None):
-      st.success("Success: Listen to your results!")
-      # Save the audio file to a specific path
-      temp_audio = os.path.join(tempfile.gettempdir(), "output.mp3")
-      tts.save(temp_audio)
+  col1, col2, col3 = st.columns(3)
+  with col2:
+    if st.button('Convert to Speech'):
+      tts = tts(text_val, language)
+      if (tts is not None):
+        st.success("Success: Listen to your results!")
+        # Save the audio file to a specific path
+        temp_audio = os.path.join(tempfile.gettempdir(), "output.mp3")
+        tts.save(temp_audio)
 
-      # Read the audio file as bytes
-      with open(temp_audio, "rb") as audio_file:
-          audio_bytes = audio_file.read()
+        # Read the audio file as bytes
+        with open(temp_audio, "rb") as audio_file:
+            audio_bytes = audio_file.read()
 
-      # Play the audio in Streamlit
-      st.audio(audio_bytes, format='audio/mp3')
-    
-    elif (text_val is None):
-      st.error("Enter Text First!")    
-    else:
-      st.error("Error: result == null :(")
-          
-  
+        # Play the audio in Streamlit
+        st.audio(audio_bytes, format='audio/mp3')
+
+      elif (text_val is None):
+        st.error("Enter Text First!")    
+      else:
+        st.error("Error: result == null :(")
+
+
 with tab2:
   st.header("Coming Soon")
 
