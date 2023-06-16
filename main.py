a8d3c36b4aeef143d41e9b2f410d98439999d9ca
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2023-06-16 13:13:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ with tab1:
   col1, col2, col3 = st.columns(3)
   with col2:
     execute = st.button('Convert to Speech')
-  if execute:
+  if (execute and (text_val != None)):
       tts = tts(text_val, language)
       if (tts is not None):
         st.success("Success: Listen to your results!")
